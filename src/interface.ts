743104140569dcf505ae5b0967e4460037bf46ae
jdohun <wjdehgns97@naver.com>
2024-01-16 10:10:58 +0900

    feat: Using Interface for functional Interface, Intersection Type
    - interface 의 활용에서 함수형 인터페이스와 교차타입에 대한 실습

--- src/interface.ts
+++ src/interface.ts
@@ -1,23 +1,59 @@
+// interface Person {
+//     firstName: string;
+//     lastName: string;
+//     age: number;
+//     // sayMyName(parameter1:string, parameter2:string):void;
+// }
+//
+// interface Employee extends Person {
+//     role: string;
+// }
+
+interface OnlyFunction {
+    (x: number, y: number): number;
+}
+
+const addTwoNum: OnlyFunction = (x, y) => x + y;
+
+let result = addTwoNum(1, 2);
+console.log(`1 + 2 = ${result}`);
+
+// const sayHello = function (person: Person): void {
+//     console.log(`Hello, ${person.firstName} ${person.lastName}!`);
+// }
+//
+// const introduce = (employee: Employee): void => {
+//     console.log(`My role is ${employee.role}, my age is ${employee.age}`)
+// }
+
+// const myFriend: Person = {firstName: "John", lastName: "Doe", age: 20};
+// sayHello(myFriend); // "Hello, John Doe!"
+//
+// const myEmployee: Employee = {firstName: "John", lastName: "Doe", age: 20, role: "member"};
+// introduce(myEmployee);
+
+interface StringArr {
+    [index: number]: string;
+}
+
+let arr: StringArr = ["a", "b", "c"];
+
+console.log(arr[1]);
+
 interface Person {
-    firstName: string;
-    lastName: string;
+    name: string;
     age: number;
 }
 
-interface Employee extends Person {
-    role: string;
+interface Developer {
+    name: string;
+    skill: string;
 }
 
-const sayHello = function (person: Person): void {
-    console.log(`Hello, ${person.firstName} ${person.lastName}!`);
-}
+type Capt = Person & Developer;
 
-const introduce = (employee: Employee): void => {
-    console.log(`My role is ${employee.role}, my age is ${employee.age}`)
-}
-
-const myFriend: Person = {firstName: "John", lastName: "Doe", age: 20};
-sayHello(myFriend); // "Hello, John Doe!"
-
-const myEmployee: Employee = {firstName: "John", lastName: "Doe", age: 20, role: "member"};
-introduce(myEmployee);
+let devPerson: Capt = {
+    name: "kim",
+    age: 777,
+    skill: "FE"
+};
