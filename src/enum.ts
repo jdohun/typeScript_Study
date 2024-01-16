9f7ec805aab3f495b6d9967d8e6bd4eb9ba04923
jdohun <wjdehgns97@naver.com>
2024-01-16 10:19:29 +0900

    feat: Enum 실습 수행

---
+++ src/enum.ts
@@ -0,0 +1,22 @@
+// 숫자형
+enum Brands {
+    Nike,		    // 0
+    Adidas,		// 1
+    NewBalance	// 2
+}
+
+const myShoes = Brands.Nike;	// 0
+
+// 문자형
+enum Player {
+    kim = '김',
+    park = '박'
+}
+
+const player = Player.park;	// 박
+
+enum Str {
+    '김','이','박','최','정'
+}
+
+const 김 = Str.김;
